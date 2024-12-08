0f0b4c330222d904dde7fa52b0c5912e85ab1f01
Pinto324 <branps18@gmail.com>
2024-12-08 13:26:05 -0600

    front 1.3

--- buildsvelte/src/routes/+page.svelte
+++ buildsvelte/src/routes/+page.svelte
@@ -15,10 +15,10 @@
   style="height: 100vh;"
 >
   <div
-    class="d-flex justify-content-center rounded align-items-center flex-column col-5 m-4 overflow-x-auto bg-light border border-secondary-subtle"
+    class="d-flex justify-content-center rounded align-items-center flex-column col-5 m-4 bg-light border border-secondary-subtle"
     style="height: 80vh;"
   >
-    <div class="w-100" style="height: 75vh;">
+    <div class="w-100 overflow-y-auto" style="height: 75vh;">
       <CodeMirror bind:value={editor} />
     </div>
     <button class="btn btn-primary m-2" on:click={parse}> Analizar</button>
